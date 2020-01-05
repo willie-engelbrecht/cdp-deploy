ba8c9f0459bc817a612841fbdd1f84c6e8470f9a
Willie Engelbrecht <wengelbrecht@hortonworks.com>
2020-01-05 15:34:29 +0000

    First commit

---
+++ cdp-deploy.sh
@@ -0,0 +1,258 @@
+# Check that we "in" our directory
+ls -l cdp-deploy.sh > /dev/null 2> /dev/null
+if [ $? -ne 0 ]
+then
+    BASEDIR=$(dirname "$0")
+    cd ${BASEDIR}
+fi
+
+# Setup some variables, here and from repo.env
+source repo.env
+export FQDNx="$(hostname -f)" # There will be an annoying space added to the end. Next command will clear it with xargs
+export FQDN=$(echo $FQDNx | xargs)
+
+# Check if the paywall username/password has values, otherwise quit the script
+export TOQUIT=0
+if [ -z "${PWALL_USER}" ];
+then
+    echo "Please set a valid username for your PWALL_USER in repo.env"
+    export TOQUIT=1
+fi
+if [ -z "${PWALL_PASS}" ];
+then
+    echo "Please set a valid password for your PWALL_PASS in repo.env"
+    export TOQUIT=1
+fi
+
+# Check that we are the root user
+whoami | grep root > /dev/null
+if [ $? -ne 0 ]
+then
+    echo "You need to run this script as the root user, or with sudo."
+    export TOQUIT=1
+fi
+
+# If any of the above failed, then we need to quit
+if [ ${TOQUIT} -eq 1 ]
+then
+    echo ""
+    echo "Exiting..."
+    exit 1
+fi
+
+# Make sure we have curl and wget installed
+yum -y install curl wget dmidecode
+
+# Set TimeZone
+ln -sf /usr/share/zoneinfo/${LOCALTZ} /etc/localtime
+
+# Find out if we are running on a specific cloud provider
+#dmidecode | grep -i 'Asset Tag: Amazon EC2'
+#if [ $? -eq 0 ] # we are on AWS
+#then
+#    export FQDN=$(curl -s ipinfo.io/hostname)
+#    if [ $? -ne 0 ]
+#    then
+#        export FQDN=$(curl -s ipinfo.io/ip)
+#    fi
+#fi
+
+# Check that we are running on CentOS7
+cat /etc/os-release | grep VERSION_ID | grep 7 > /dev/null;
+if [ $? -ne 0 ]
+then
+    echo "This system must be a CentOS7/RHEL7 based installation."
+    echo ""
+    echo "Suggested cloud image names:"
+    echo "AWS: ami-ee6a718a "
+    echo ""
+    echo "Quitting...."
+    exit 1;
+fi
+
+# Disable SELinux
+setenforce 0 > /dev/null 2> /dev/null
+sed -i 's/enforcing/disabled/' /etc/sysconfig/selinux
+
+# Disable Transparent Hugepages
+echo never > /sys/kernel/mm/transparent_hugepage/defrag
+echo never > /sys/kernel/mm/transparent_hugepage/enabled
+
+# Generate a 12 char random password
+RAND_STRING="a$(echo "$(date)$(hostname)" | md5sum);"
+RAND_PW=$(echo ${RAND_STRING:0:12})
+
+# Setup PostgreSQL 10 repo, and install
+yum -y install https://yum.postgresql.org/10/redhat/rhel-7-x86_64/pgdg-centos10-10-2.noarch.rpm
+yum -y install postgresql10-server postgresql10
+wget -O - https://jdbc.postgresql.org/download/postgresql-42.2.9.jar > /usr/lib/postgresql-jdbc.jar
+/usr/pgsql-10/bin/postgresql-10-setup initdb
+
+# Allow listeners from any host
+sed -e 's,#listen_addresses = \x27localhost\x27,listen_addresses = \x27*\x27,g' -i /var/lib/pgsql/10/data/postgresql.conf
+
+# Increase number of connections
+sed -e 's,max_connections = 100,max_connections = 300,g' -i  /var/lib/pgsql/10/data/postgresql.conf
+
+# Save the original & replace with a new pg_hba.conf
+mv /var/lib/pgsql/10/data/pg_hba.conf /var/lib/pgsql/10/data/pg_hba.conf.backup
+
+# Setup the new pg_hba.conf file
+cat > /var/lib/pgsql/10/data/pg_hba.conf << EOF
+  # TYPE  DATABASE        USER            ADDRESS                 METHOD
+  local   all             all                                     peer
+  host    scm             scm             0.0.0.0/0               md5
+  host    das             das             0.0.0.0/0               md5
+  host    hive            hive            0.0.0.0/0               md5
+  host    hue             hue             0.0.0.0/0               md5
+  host    oozie           oozie           0.0.0.0/0               md5
+  host    ranger          rangeradmin     0.0.0.0/0               md5
+  host    rman            rman            0.0.0.0/0               md5
+  host    streamsmsgmgr   streamsmsgmgr   0.0.0.0/0               md5
+  host    registry        registry        0.0.0.0/0               md5
+EOF
+
+chown postgres:postgres /var/lib/pgsql/10/data/pg_hba.conf;
+chmod 600 /var/lib/pgsql/10/data/pg_hba.conf
+
+# Enable and start PostgreSQL
+systemctl enable postgresql-10.service
+systemctl start postgresql-10.service
+
+# Create a DDL file for all our DBs
+cat > /tmp/create_ddl_cdp.sql << EOF
+CREATE ROLE activitymonitor LOGIN PASSWORD 'supersecret1';
+CREATE ROLE das LOGIN PASSWORD 'supersecret1';
+CREATE ROLE hive LOGIN PASSWORD 'supersecret1';
+CREATE ROLE hue LOGIN PASSWORD 'supersecret1';
+CREATE ROLE oozie LOGIN PASSWORD 'supersecret1';
+CREATE ROLE rangeradmin LOGIN PASSWORD 'supersecret1';
+CREATE ROLE rman LOGIN PASSWORD 'supersecret1';
+CREATE ROLE scm LOGIN PASSWORD 'supersecret1';
+CREATE ROLE streamsmsgmgr LOGIN PASSWORD 'supersecret1';
+CREATE ROLE registry LOGIN PASSWORD 'supersecret1';
+CREATE DATABASE activitymonitor OWNER das ENCODING 'UTF-8';
+CREATE DATABASE das OWNER das ENCODING 'UTF-8';
+CREATE DATABASE hive OWNER hive ENCODING 'UTF-8';
+CREATE DATABASE hue OWNER hue ENCODING 'UTF-8';
+CREATE DATABASE oozie OWNER oozie ENCODING 'UTF-8';
+CREATE DATABASE ranger OWNER rangeradmin ENCODING 'UTF-8';
+CREATE DATABASE rman OWNER rman ENCODING 'UTF-8';
+CREATE DATABASE scm OWNER scm ENCODING 'UTF-8';
+CREATE DATABASE streamsmsgmgr OWNER scm ENCODING 'UTF-8';
+CREATE DATABASE registry OWNER scm ENCODING 'UTF-8';
+EOF
+
+# Run the sql file to create the schema for all DB’s
+su - postgres -c 'psql < /tmp/create_ddl_cdp.sql'
+
+# Install additional dependencies
+yum --nogpgcheck -y install java-1.8.0-openjdk-devel mlocate telnet at jq rng-tools vim nc python3-pip.noarch
+
+# We'll need atd later on to schedule additional steps
+systemctl enable atd
+systemctl start atd
+
+systemctl enable rngd
+systemctl start rngd
+
+# Download Cloudera Manager repo and install
+wget $CM_BASEURL/cloudera-manager.repo -P /etc/yum.repos.d/
+sed -e "s,username=changeme,username="${PWALL_USER}",g" -i /etc/yum.repos.d/cloudera-manager.repo
+sed -e "s,password=changeme,password="${PWALL_PASS}",g" -i /etc/yum.repos.d/cloudera-manager.repo
+sed -i "s/archive/${PWALL_USER}:${PWALL_PASS}@archive/g" /etc/yum.repos.d/cloudera-manager.repo
+
+# Import the GPG Key
+rpm --import $CM_BASEURL/RPM-GPG-KEY-cloudera
+
+# Install CM Server & Agent
+if [ ${LOCALREPO} -eq 1 ]
+then
+    yum -y install ${LOCAL_CM_SERVER} cloudera-manager-agent
+    yum -y install cloudera-manager-server
+else
+    yum -y install cloudera-manager-server cloudera-manager-agent
+fi
+
+# Run the prepare script for SCM db
+/opt/cloudera/cm/schema/scm_prepare_database.sh postgresql scm scm supersecret1
+
+# Setup AutoTLS for Cloudera Manager and Services
+#JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk/ /opt/cloudera/cm-agent/bin/certmanager --location /opt/cloudera/CMCA setup --configure-services
+
+# Download CSDs to /opt/cloudera/csd
+if [ ${CDPONLY} -eq 0 ]
+then
+    if [ -n "${CFM_NIFI_CSD_URL}" ]; then wget ${CFM_NIFI_CSD_URL} -P /opt/cloudera/csd/; fi
+    if [ -n "${STREAMS_MESSAGING_MANAGER_CSD_URL}" ]; then wget ${STREAMS_MESSAGING_MANAGER_CSD_URL} -P /opt/cloudera/csd/; fi
+    if [ -n "${SCHEMAREGISTRY_CSD_URL}" ]; then wget ${SCHEMAREGISTRY_CSD_URL} -P /opt/cloudera/csd/; fi
+    if [ -n "${CFM_NIFIREG_CSD_URL}" ]; then wget ${CFM_NIFIREG_CSD_URL} -P /opt/cloudera/csd/; fi
+    chown cloudera-scm:cloudera-scm -R /opt/cloudera/csd/*
+fi
+
+# Start the Cloudera Node Agent
+systemctl enable cloudera-scm-agent
+systemctl start cloudera-scm-agent
+
+# Start the CM Server
+systemctl enable cloudera-scm-server
+systemctl start cloudera-scm-server
+echo "Please wait while Cloudera Manager Server starts up:"
+
+# Wait for CM to run on port 7183
+RET=1
+ while [ $RET -eq 1 ]; do
+  echo -n .; sleep 2
+  echo "" | nc -v localhost 7180 > /dev/null 2> /dev/null
+  RET=$?
+done
+
+# Setup the template for installation
+if [ ${CDPONLY} -eq 1 ]
+then
+    # Install only Base CDP, no CDF
+    cat cluster_template_base.json > /tmp/cluster_template.json
+else
+    # Install CDP and CDF
+   cat cluster_template_full.json > /tmp/cluster_template.json
+fi
+sed  -i "s/xxFQDNxx/${FQDN}/g" /tmp/cluster_template.json
+sed  -i "s/xxPWxx/supersecret1/g" /tmp/cluster_template.json
+
+# Setup Cloudera Manager Python3 SDK
+pip3 install cm_client
+if [ ${LOCALREPO} -eq 1 ]
+then
+    python3 cm_install.py --hostname $FQDN --cdponly ${CDPONLY} --localrepo ${LOCALREPO} --parcelsuri ${LOCAL_PARCELS}
+else
+    if [ -n "${REMOTE_PARCELS}" ]
+    then
+        python3 cm_install.py --hostname $FQDN --cdponly ${CDPONLY} --localrepo ${LOCALREPO} --parcelsuri ${REMOTE_PARCELS}
+    else
+        python3 cm_install.py --hostname $FQDN --cdponly ${CDPONLY} --localrepo ${LOCALREPO}
+    fi
+fi
+
+echo ""
+echo "Add the following to your hosts file:" | tee -a /root/cm_install.txt
+ipaddr=$(hostname -I | awk '{print $1}')
+echo ${ipaddr} | grep '192.168.0.' > /dev/null
+if [ $? -eq 0 ]
+then
+    echo "${ipaddr} $(hostname -f)" | tee -a /root/cm_install.txt
+else
+    ipaddr=$(curl -s ipinfo.io/ip)
+    echo "${ipaddr} ${FQDN}" | tee -a /root/cm_install.txt
+fi
+echo "" | tee -a /root/cm_install.txt
+
+echo "You can now browse to: http://${FQDN}:7180" | tee -a /root/cm_install.txt
+echo "username: admin" | tee -a /root/cm_install.txt
+echo "password: admin" | tee -a /root/cm_install.txt
+
+echo "echo '" >> /root/.bash_profile
+cat /root/cm_install.txt  >> /root/.bash_profile
+echo "'" >> /root/.bash_profile
+
+echo "Cluster installation will take a little while to complete"
+echo ""
---
+++ cm_install.py
@@ -0,0 +1,190 @@
+#!/bin/env python3 
+import cm_client
+import json
+import hashlib
+import time
+from optparse import OptionParser
+from cm_client.rest import ApiException
+from collections import namedtuple
+from pprint import pprint
+
+# https://cloudera.github.io/cm_api/docs/python-client-swagger/
+# https://archive.cloudera.com/cm7/7.0.3/generic/jar/cm_api/swagger-html-sdk-docs/python/README.html
+
+# https://github.com/mkby/myinstaller/blob/master/deploy_cdh.py
+# https://gist.github.com/gdgt/d2eb4abe39da05353a58451c103f41e5
+
+hostname = None
+password = None
+localrepo = False
+parcelsuri = ''
+
+# Configure HTTP basic authorization: basic
+cm_client.configuration.username = 'admin'
+cm_client.configuration.password = 'admin'
+
+# Path of truststore file in PEM
+cm_client.configuration.verify_ssl = True
+cm_client.configuration.ssl_ca_cert = '/var/lib/cloudera-scm-server/certmanager/trust-store/cm-auto-in_cluster_ca_cert.pem'
+
+# Construct base URL for API
+api_host = 'http://localhost'
+port = '7180'
+api_version = 'v40'
+api_url = api_host + ':' + port + '/api/' + api_version
+api_client = cm_client.ApiClient(api_url)
+
+
+def saveClusterTemplate():
+    cluster_api_instance = cm_client.ClustersResourceApi(api_client)
+
+    # Lists all known clusters.
+    api_response = cluster_api_instance.read_clusters(view='SUMMARY')
+    cluster_name = ''
+    for cluster in api_response.items:
+        cluster_name = cluster.name
+        template = cluster_api_instance.export(cluster_name)
+
+        json_dict = api_client.sanitize_for_serialization(template)
+        with open('/tmp/cluster_template.json', 'w') as out_file:
+            json.dump(json_dict, out_file, indent=4, sort_keys=True)
+
+        print(cluster.name, "-", cluster.full_version)
+
+def acceptTrial():
+    api_instance = cm_client.ClouderaManagerResourceApi(api_client)
+
+    try:
+        api_instance.begin_trial()
+    except ApiException as e:
+        print("Exception when calling ClouderaManagerResourceApi->begin_trial: %s\n" % e)
+
+def loadLocalRepo():
+    new_parcel_repo_urls = parcelsuri
+
+    cm_api_instance = cm_client.ClouderaManagerResourceApi(api_client)
+    cm_configs = cm_api_instance.get_config(view='full')
+    
+    new_cm_config = cm_client.ApiConfig(name='REMOTE_PARCEL_REPO_URLS', value=new_parcel_repo_urls)
+    new_cm_configs = cm_client.ApiConfigList([new_cm_config])
+    updated_cm_configs = cm_api_instance.update_config(body=new_cm_configs)
+
+def appendRemoteRepo():
+    new_parcel_repo_urls = parcelsuri
+
+    cm_api_instance = cm_client.ClouderaManagerResourceApi(api_client)
+    cm_configs = cm_api_instance.get_config(view='full')
+    old_parcel_repo_urls = None
+    for cm_config in cm_configs.items:
+        if cm_config.name == 'REMOTE_PARCEL_REPO_URLS':
+            old_parcel_repo_urls = cm_config.value
+
+    new_parcel_repo_urls = old_parcel_repo_urls + ',' + parcelsuri
+    cm_api_instance = cm_client.ClouderaManagerResourceApi(api_client)
+    cm_configs = cm_api_instance.get_config(view='full')
+
+    new_cm_config = cm_client.ApiConfig(name='REMOTE_PARCEL_REPO_URLS', value=new_parcel_repo_urls)
+    new_cm_configs = cm_client.ApiConfigList([new_cm_config])
+    updated_cm_configs = cm_api_instance.update_config(body=new_cm_configs)
+
+def loadTemplateThenInstall():
+    # Load the updated cluster template
+    with open('/tmp/cluster_template.json') as in_file:
+        json_str = in_file.read()
+    # Following step is used to deserialize from json to python API model object
+    Response = namedtuple("Response", "data")
+    dst_cluster_template=api_client.deserialize(response=Response(json_str), response_type=cm_client.ApiClusterTemplate)
+
+    cm_api_instance = cm_client.ClouderaManagerResourceApi(api_client)
+    command = cm_api_instance.import_cluster_template(body=dst_cluster_template)
+
+def get_host_resource(hostname):
+    api_instance = cm_client.HostsResourceApi(api_client)
+    try:
+        # Returns the hostIds for all hosts in the system.
+        # api_response = api_instance.read_hosts(view=view)
+        api_host_response = [x for x in api_instance.read_hosts(view='summary').items
+                             if hostname == x.hostname]
+    except ApiException as e:
+        print("Exception when calling HostsResourceApi->read_hosts: %s\n" % e)
+
+    return api_host_response[0]
+
+def setupCMS():
+    service_name = 'mgmt'
+    service_type = 'MGMT'.upper()
+
+    api_instance = cm_client.MgmtServiceResourceApi(api_client)
+
+    services_instance = cm_client.MgmtServiceResourceApi(api_client)
+    mgmt_role_instance = cm_client.MgmtRolesResourceApi(api_client)
+    rcg_instance = cm_client.MgmtRoleConfigGroupsResourceApi(api_client)
+
+    services_instance.setup_cms(body=cm_client.ApiService(name=service_name, type=service_type, display_name='Cloudera Management Service'))
+    host_id = getattr(get_host_resource(hostname), 'host_id', None)
+
+    role_list = []
+    for role_type in ["REPORTSMANAGER", "EVENTSERVER", "HOSTMONITOR", "ALERTPUBLISHER", "SERVICEMONITOR"]:
+        role_name = service_name + "-" + role_type + "-" + hashlib.md5(hostname.encode('utf-8')).hexdigest()
+        role_list.append({"name": role_name, "type": role_type, "hostRef": {"hostId": host_id}})
+
+    body = cm_client.ApiRoleList(role_list)
+    api_response = mgmt_role_instance.create_roles(body=body)
+
+    api_response = rcg_instance.read_role_config_groups()
+    for rcg in api_response.items:
+        if rcg.role_type == 'REPORTSMANAGER':
+            rcg_instance.update_config(rcg.name, message=None,
+                                           body=cm_client.ApiConfigList([
+                                                   {'name': 'headlamp_database_host', 'value': hostname},
+                                                   {'name': 'headlamp_database_name', 'value': 'rman'},
+                                                   {'name': 'headlamp_database_user', 'value': 'rman'},
+                                                   {'name': 'headlamp_database_password', 'value': 'supersecret1'},
+                                                   {'name': 'headlamp_database_type', 'value': 'postgresql'}
+                                               ]))
+
+    services_instance.start_command()
+
+if __name__ == "__main__":
+    optp = OptionParser()
+    optp.add_option("-n", "--hostname",     dest="hostname",     help="Hostname of the system")
+    optp.add_option("-b", "--cdponly" ,     dest="cdponly",      help="Install only CDP")
+    optp.add_option("-l", "--localrepo",    dest="localrepo",    help="Use local repositories")
+    optp.add_option("-c", "--parcelsuri",   dest="parcelsuri",   help="URLS of parcel location")
+    optp.add_option("-p", "--password",     dest="password",     help="Password to user")
+    opts, args = optp.parse_args()
+
+    if opts.hostname:
+        hostname = str(opts.hostname)
+
+    if opts.localrepo:
+        localrepo = opts.localrepo
+
+    if opts.parcelsuri:
+        parcelsuri = opts.parcelsuri
+
+    if opts.password:
+        password = opts.password
+
+    print("Hostname: " + hostname)
+
+    print("Accepting the trial license")
+    acceptTrial()
+
+    if str(localrepo) == '1':
+        if str(parcelsuri) != '': 
+            print("Load a new local parcel repository location")
+            loadLocalRepo()
+    else:
+        if str(parcelsuri) != '': 
+            print("Adding additional remote parcel repos")
+            appendRemoteRepo()
+
+    print("Sleeping for 10 seconds")
+    time.sleep(10)
+    
+    print("Loading template then starting automated install")
+    loadTemplateThenInstall()
+    
+    print("Setting up Cloudera Management Service")
+    setupCMS()
